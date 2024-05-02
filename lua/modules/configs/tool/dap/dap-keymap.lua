08a7dd91093d7e129ea297b2074632bcdf52b194
Jintao Guan <jintaoguan0817@gmail.com>
2024-05-01 23:35:21 -0700

    change the dap key mapping to avoid conflict

--- lua/modules/configs/tool/dap/dap-keymap.lua
+++ lua/modules/configs/tool/dap/dap-keymap.lua
@@ -5,7 +5,7 @@ local map_cmd = bind.map_cmd
 
 local did_load_debug_mappings = false
 local debug_keymap = {
-	["nv|K"] = map_cmd("<Cmd>lua require('dapui').eval()<CR>")
+	["nv|L"] = map_cmd("<Cmd>lua require('dapui').eval()<CR>")
 		:with_noremap()
 		:with_nowait()
 		:with_desc("Evaluate expression under cursor"),
